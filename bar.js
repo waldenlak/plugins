8ce67dfd01df0568c26438b0ab9c8c5b57e26871
waldenlak <79695726+waldenlak@users.noreply.github.com>
2021-10-07 11:47:06 +0800

    分类条、404文章条、滚动条

---
+++ bar.js
@@ -0,0 +1,67 @@
+categoriesBarActive()
+topCategoriesBarScroll()
+
+//分类条
+function categoriesBarActive(){
+  var urlinfo = window.location.pathname;
+  urlinfo = decodeURIComponent(urlinfo)
+  console.log(urlinfo);
+  //判断是否是首页
+  if (urlinfo == '/'){
+    if (document.querySelector('#categorybar')){
+      document.getElementById('home').classList.add("select")
+    }
+  }else {
+    // 验证是否是分类链接
+    var pattern = /\/categories\/.*?\//;
+    var patbool = pattern.test(urlinfo);
+    console.log(patbool);
+    // 获取当前的分类
+    if (patbool) {
+      var valuegroup = urlinfo.split("/");
+      console.log(valuegroup[2]);
+      // 获取当前分类
+      var nowCategorie = valuegroup[2];
+      if (document.querySelector('#categorybar')){
+        document.getElementById(nowCategorie).classList.add("select");
+      }
+    }
+  }
+  
+}
+
+//鼠标控制横向滚动
+function topCategoriesBarScroll(){
+  if (document.getElementById("categorybar-items")){
+    let xscroll = document.getElementById("categorybar-items");
+  xscroll.addEventListener("mousewheel", function (e) {
+    //计算鼠标滚轮滚动的距离
+    let v = -e.wheelDelta / 2;
+    xscroll.scrollLeft += v;
+    //阻止浏览器默认方法
+    e.preventDefault();
+}, false);
+  }
+}
+
+
+//滚动条自动隐藏
+var t1 = 0;
+var t2 = 0;
+var timer = null; // 定时器
+document.styleSheets[0].insertRule('*::-webkit-scrollbar-thumb {display:none;}', 0);
+
+// scroll监听
+document.onscroll = function() {
+  clearTimeout(timer);
+  timer = setTimeout(isScrollEnd, 1000);
+  t1 = document.documentElement.scrollTop || document.body.scrollTop;
+  document.styleSheets[0].insertRule('*::-webkit-scrollbar-thumb {display:block;}', 0);
+}
+
+function isScrollEnd() {
+  t2 = document.documentElement.scrollTop || document.body.scrollTop;
+  if(t2 == t1){
+    document.styleSheets[0].insertRule('*::-webkit-scrollbar-thumb {display:none;}', 0);
+  }
+}
